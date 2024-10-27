273c69ca4a42fe5bfb46ac5bfa2785b9e475b702
PS75215 <p.saha@zensar.com>
2024-10-27 22:11:25 +0530

    java-script setup

---
+++ test.js
@@ -0,0 +1,6 @@
+console.log("Hi EveryOne");
+// To run Java Script file we need to install Node JS
+// Power shell command -> file location node test.js
+// CMD command -> C:\Program Files\nodejs\node.exe test.js
+//   C:\Program Files\nodejs\node.exe -> Node applicaion location and  .\test.js -> file name
+//  to simplify run we can install
